b09a3ee22b46710a38bfe89fa13322594bd422cf
David Blacksher <db0204585@otc.edu>
2017-09-26 14:37:40 -0500

    David's Function
    Just my function and Megan's image in my p tag to show that JS will
    override the in line HTML.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ function className(){
 }
 
 window.addEventListener("load", className, false);
+
+//David's function
+
+function tagName(){
+	var phone = document.getElementsByTagName("p");
+	phone[0].innerHTML = "<img class='rounded-circle' src='phone.jpg' alt='Corgi on phone' width='240' height='240'>";
+
+}
+window.addEventListener("load", tagName, false);
