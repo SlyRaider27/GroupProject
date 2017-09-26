6500483e7a8f75f2296f299709e6458ad2cc1198
Megan Britt <mb0783333@otc.edu>
2017-09-25 19:52:44 -0500

    Megan's function complete. I am using the first div for images.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+//Megan's Function
+function className(){
+	var pic = document.getElementsByClassName("bork");
+	pic[0].innerHTML = "<img class='rounded-circle' src='wink.jpg' width='240' height='240'>";
+}
 
+window.addEventListener("load", className, false);
